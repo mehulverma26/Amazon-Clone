126879a13b671c0a6b029f14504ae904b1e8f1e2
Mehul Verma <mehulverma04@gmail.com>
2023-04-14 22:00:37 +0530

    /

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.prevertDefault();
+  };
+  const register = (e) => {
+    e.prevertDefault();
+  };
+
   return (
     <>
       <div className="login">
@@ -17,13 +26,27 @@ function Login() {
           <h1>Sign in</h1>
           <form>
             <h5>Email</h5>
-            <input type="text" />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <h5>Password</h5>
-            <input type="password" />
-            <button className="login__signInButton">Sign In</button>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="submit"
+              onClick={signIn}
+              className="login__signInButton"
+            >
+              Sign In
+            </button>
           </form>
           <p>*T&C apply</p>
-          <button className="login__registerButton">
+          <button onClick={register} className="login__registerButton">
             Create your Amazon Account
           </button>
         </div>
